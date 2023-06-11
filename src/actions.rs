8fea8b601e6d56c7896e43234790abafd2145166
ZhengjunHUO <firelouiszj@hotmail.com>
2023-06-11 09:51:52 +0000

    rearrange func in module
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- src/actions.rs
+++ src/actions.rs
@@ -1,3 +1,7 @@
+mod firewall {
+    include!("cgroup_fw.skel.rs");
+}
+
 use super::*;
 use crate::rule::*;
 use crate::sys::*;
@@ -5,6 +9,7 @@ use crate::utils::*;
 use anyhow::Result;
 use clap::Args;
 use crossbeam_channel::{select, tick};
+use firewall::*;
 use libbpf_rs::{Link, Map, MapFlags};
 use std::path::Path;
 use std::time::Duration;
@@ -31,6 +36,107 @@ pub struct Protocol {
     /// specify a udp port
     #[arg(long, value_name = "UDP_PORT", value_parser = clap::value_parser!(u16).range(1001..))]
     udp: Option<u16>,
+}
+
+pub fn prepare_ctn_dir(ctn_id: &str) -> Result<()> {
+    use std::fs::create_dir;
+    use std::os::fd::AsRawFd;
+
+    // (1) Create dir for container
+    let ctn_dir = get_ctn_bpf_path(ctn_id);
+    let ctn_dir_path = Path::new(&ctn_dir);
+    if ctn_dir_path.is_dir() {
+        // return if the dir is already there
+        //println!("[DEBUG] Dir {:?} already exists.", ctn_dir_path);
+        return Ok(());
+    }
+
+    create_dir(ctn_dir_path)?;
+
+    // (2) Load bpf programs and maps
+    let builder = CgroupFwSkelBuilder::default();
+    increase_rlimit()?;
+    // Get an opened, pre-load bpf object
+    let open = builder.open()?;
+    // Get a loaded bpf object
+    let mut obj = open.load()?;
+
+    // Get target cgroup id
+    let f = std::fs::OpenOptions::new()
+        .read(true)
+        .write(false)
+        .open(format!(
+            "/sys/fs/cgroup/system.slice/docker-{}.scope",
+            ctn_id
+        ))?;
+    let cgroup_fd = f.as_raw_fd();
+
+    // (2.a) Get loaded programs and attach to the cgroup, then pin to the fs
+    let mut eg_link = obj.progs_mut().egress_filter().attach_cgroup(cgroup_fd)?;
+    // The prog_type and attach_type are inferred from the c program
+    // should be CgroupInetEgress here
+    //println!("[DEBUG]: Attach type is {:?}", obj.progs().egress_filter().attach_type());
+    eg_link.pin(format!("{}/{}", &ctn_dir, EGRESS_LINK_NAME))?;
+
+    let mut ig_link = obj.progs_mut().ingress_filter().attach_cgroup(cgroup_fd)?;
+    ig_link.pin(format!("{}/{}", &ctn_dir, INGRESS_LINK_NAME))?;
+
+    // (2.b) Get loaded maps and pin to the fs
+    let mut maps = obj.maps_mut();
+
+    let eg_fw_map = maps.egress_blacklist();
+    // Persist the map on bpf vfs
+    eg_fw_map.pin(format!("{}/{}", &ctn_dir, EGRESS_MAP_NAME))?;
+
+    let eg_l4_fw_map = maps.egress_l4_blacklist();
+    eg_l4_fw_map.pin(format!("{}/{}", &ctn_dir, EGRESS_L4_MAP_NAME))?;
+
+    let ig_fw_map = maps.ingress_blacklist();
+    ig_fw_map.pin(format!("{}/{}", &ctn_dir, INGRESS_MAP_NAME))?;
+
+    let ig_l4_fw_map = maps.ingress_l4_blacklist();
+    ig_l4_fw_map.pin(format!("{}/{}", &ctn_dir, INGRESS_L4_MAP_NAME))?;
+
+    let data_flow_map = maps.data_flow();
+    data_flow_map.pin(format!("{}/{}", &ctn_dir, DATAFLOW_MAP_NAME))?;
+
+    Ok(())
+}
+
+pub fn free_ctn_resources(ctn_name: &str) -> Result<()> {
+    use std::fs::remove_dir;
+
+    let ctn_id = get_ctn_id_from_name(&ctn_name)?;
+    let ctn_dir = get_ctn_bpf_path(&ctn_id);
+    let ctn_dir_path = Path::new(&ctn_dir);
+    if !ctn_dir_path.try_exists()? {
+        // return if the dir is already there
+        //println!("[DEBUG] Dir {:?} already deleted.", ctn_dir_path);
+        return Ok(());
+    }
+
+    let all_links = vec![EGRESS_LINK_NAME, INGRESS_LINK_NAME];
+    let all_maps = get_all_maps();
+
+    // if link is unpinned and map stays, the rules will not applied any more.
+    for l in all_links {
+        let path = format!("{}/{}", ctn_dir, l);
+        let mut prog = Link::open(path)?;
+        prog.unpin()?;
+        println!("[DEBUG] Unpinned link {}", l);
+    }
+
+    // if map is unpinned and link stays, the rules is still in effect ?!
+    for m in all_maps {
+        let path = format!("{}/{}", ctn_dir, m);
+        let mut map = Map::from_pinned_path(&path)?;
+        map.unpin(&path)?;
+        println!("[DEBUG] Unpinned map {}", m);
+    }
+
+    remove_dir(ctn_dir_path)?;
+
+    Ok(())
 }
 
 pub fn update_rule(
@@ -147,42 +253,6 @@ pub fn show_rules(ctn_name: &str) -> Result<()> {
         }
         println!("");
     }
-
-    Ok(())
-}
-
-pub fn free_ctn_resources(ctn_name: &str) -> Result<()> {
-    use std::fs::remove_dir;
-
-    let ctn_id = get_ctn_id_from_name(&ctn_name)?;
-    let ctn_dir = get_ctn_bpf_path(&ctn_id);
-    let ctn_dir_path = Path::new(&ctn_dir);
-    if !ctn_dir_path.try_exists()? {
-        // return if the dir is already there
-        //println!("[DEBUG] Dir {:?} already deleted.", ctn_dir_path);
-        return Ok(());
-    }
-
-    let all_links = vec![EGRESS_LINK_NAME, INGRESS_LINK_NAME];
-    let all_maps = get_all_maps();
-
-    // if link is unpinned and map stays, the rules will not applied any more.
-    for l in all_links {
-        let path = format!("{}/{}", ctn_dir, l);
-        let mut prog = Link::open(path)?;
-        prog.unpin()?;
-        println!("[DEBUG] Unpinned link {}", l);
-    }
-
-    // if map is unpinned and link stays, the rules is still in effect ?!
-    for m in all_maps {
-        let path = format!("{}/{}", ctn_dir, m);
-        let mut map = Map::from_pinned_path(&path)?;
-        map.unpin(&path)?;
-        println!("[DEBUG] Unpinned map {}", m);
-    }
-
-    remove_dir(ctn_dir_path)?;
 
     Ok(())
 }
